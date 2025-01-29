3f3477bc53642dd295a6dc899265fb54d6a12112
soviljn <sovilj94@gmail.com>
2025-01-29 10:52:57 +0100

    izmjene

---
+++ samples/BlazorWebAssembly/wwwroot/js/app.js
@@ -0,0 +1,20 @@
+﻿window.HelloWorld = () => {
+    console.log('Hello World from app.js');
+}
+
+window.AddClass = () => {   
+    console.log("Add class");
+}
+
+window.GetBoundingClientRect = function (elementId) {
+    console.log(`Trying to get bounding client rect for element with id: ${elementId}`);
+    var element = document.getElementById(elementId);
+    if (element) {
+        var rect = element.getBoundingClientRect();
+        console.log('Bounding client rect:', rect);
+        return rect;
+    } else {
+        console.error(`Element with id '${elementId}' not found.`);
+        return null;
+    }
+}
